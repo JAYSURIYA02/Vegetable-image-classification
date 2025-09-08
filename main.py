83aabfe4e81a81aa650facb2bfc6655c20c922ad
JAYSURIYA02 <jaysuriya.rv@gmail.com>
2025-09-08 20:20:45 +0530

    change main

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ categories = os.listdir(path)
 for category in categories:
   terms = os.path.join(path, category)
   vege_name = os.listdir(terms)
-  for vege in vege_name:
+  for vege_ind,vege in enumerate(vege_name):
     inpath = os.path.join(terms, vege)
     for img in os.listdir(inpath):
       img_array = cv2.imread(os.path.join(inpath, img))
       resized_array = cv2.resize(img_array, (100, 100))
       flattened_array = resized_array.flatten()
+      data.append(flattened_array)
+      labels.append(vege_ind)
 
 
 
