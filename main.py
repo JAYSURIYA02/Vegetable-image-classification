566399685d7289e4819a5dd4404a7f5a6bd379ef
JAYSURIYA02 <jaysuriya.rv@gmail.com>
2025-09-08 20:16:05 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import cv2
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+path = r"Vegetable_images"
+categories = os.listdir(path)
+for category in categories:
+  terms = os.path.join(path, category)
+  vege_name = os.listdir(terms)
+  for vege in vege_name:
+    inpath = os.path.join(terms, vege)
+    for img in os.listdir(inpath):
+      img_array = cv2.imread(os.path.join(inpath, img))
+      resized_array = cv2.resize(img_array, (100, 100))
+      flattened_array = resized_array.flatten()
+      
+
+    
+ 
